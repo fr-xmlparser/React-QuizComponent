3c6b925647564441d310049489d34a313106d7cd
unknown <treyhay@gmail.com>
2018-06-25 19:35:56 -0400

    fixing small bug

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react'
 
 class QuizQuesion extends Component {
-
-	constructor(props) {
-		super(props)
-	}
-
 	render(props) {
 		return (
 			<main>
@@ -20,7 +15,6 @@ class QuizQuesion extends Component {
 			</main>
 		)
 	}
-
 }
 
 export default QuizQuesion
